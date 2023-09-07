fe7620a5cab7403b8cc347f18637415f79e3aacc
Siddhant Uniyal <siddhantuniyal416@gmail.com>
2023-09-07 17:30:39 +0530

    Added error handling,syn,anto and comments

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ import express from "express";
 
 const app = express();
 
+//middleware - Piece of software sitting between the req and res
 //middleware for parsing of json
 app.use(express.json());
 
 //creating a server
-app.listen(5000, () => {});
+app.listen(2000, () => {});
 
 //For dynamic entry of words
 app.get("/:word", async (req, res) => {
@@ -26,27 +27,28 @@ app.get("/:word", async (req, res) => {
     const data = await response.json();
 
     //if api doesn't have desired word
-    if (response.status===404) {
-      res.status(404).json({ error: "Word not found in the dictionary" });
-
-      return;
+    if (!response) {
+      return res
+        .status(404)
+        .json({ error: "Word not found in the dictionary" });
     }
 
     const flashcard = [];
     const information = data[0];
-    const definition = information.meanings[0]?.definitions[0]?.definition; //many words have empty values in some fields. if i don't use ? it will return undefined
+    const definition = information.meanings[0]?.definitions[0]?.definition; //incase of empty values anywhere
+    const synonyms = information.meanings[0]?.synonyms[0];
+    const antonyms = information.meanings[0]?.antonyms[0];
 
     flashcard.push({
       word,
       definition,
+      synonyms,
+      antonyms,
     });
 
-    res.setHeader("Content-Type", "application/json"); //telling that our response is a json
-
     res.status(200).json(flashcard); //sending our flashcard with a success status code
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal Server Error" });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Error" });
   }
 });
-
